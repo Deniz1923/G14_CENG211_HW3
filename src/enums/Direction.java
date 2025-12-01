352ed2ab3232ff11df066fd3eb18a73925de0ed5
Deniz1923 <ozgurdeniz2018@gmail.com>
2025-12-01 20:25:02 +0300

    Implement Direction.java, Add Notation into FoodType.java

--- src/enums/Direction.java
+++ src/enums/Direction.java
@@ -1,8 +1,14 @@
 package enums;
 
 public enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
+    UP, DOWN, LEFT, RIGHT;
+
+    public Direction opposite() {
+        return switch (this) {
+            case UP -> DOWN;
+            case DOWN -> UP;
+            case LEFT -> RIGHT;
+            case RIGHT -> LEFT;
+        };
+    }
 }
--- src/enums/FoodType.java
+++ src/enums/FoodType.java
@@ -1,9 +1,19 @@
 package enums;
 
 public enum FoodType {
-    KRILL,
-    CRUSTACEAN,
-    ANCHOVY,
-    SQUID,
-    MACKEREL
+    KRILL("Kr"),
+    CRUSTACEAN("Cr"),
+    ANCHOVY("An"),
+    SQUID("Sq"),
+    MACKEREL("Ma");
+
+    private final String notation;
+
+    FoodType(String notation) {
+        this.notation = notation;
+    }
+
+    public String getNotation() {
+        return notation;
+    }
 }
